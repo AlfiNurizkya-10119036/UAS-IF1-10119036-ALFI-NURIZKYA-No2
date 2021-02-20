d51d61ef7e35c4282e757edbfd768b917ed1ae9d
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2021-02-20 13:00:12 +0700

    UAS PBO

---
+++ src/uas/if1/pkg10119036/alfinurizkya/no2/UASIF110119036ALFINURIZKYANO2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if1.pkg10119036.alfinurizkya.no2;
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * Nama     : ALFI NURIZKYA
+ * Kelas    : IF-1
+ * NIM      : 10119036
+ * Deskripsi Program :
+ */
+public class UASIF110119036ALFINURIZKYANO2 {
+    public static void main(String[] args) {
+        int saldoAwal = 100000;
+        for (int i = 1; i < 6; i++) {
+            saldoAwal += 500;
+            System.out.println("Saldo di bulan ke-" + i + " : " + saldoAwal);
+        }
+        
+    }
+    
+}
+    
